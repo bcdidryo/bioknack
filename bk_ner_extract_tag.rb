d46ae5b2acb2d1148f967f08e9ee22c30cbf9b3b
Joachim Baran <joachim.baran@gmail.com>
2012-12-08 10:41:57 -0500

    Fixed indentation and added a "max" integer for exitting after n-matches. That means documents do not have to be read as a whole anymore.

--- bk_ner_extract_tag.rb
+++ bk_ner_extract_tag.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 
-if ARGV.length != 2 then
+if ARGV.length < 2 or ARGV.length > 3 then
 	# TODO
-        exit
+	exit
 end
 
 opentag = ARGV[0]
 closetag = ARGV[1]
+match = 0
+match = ARGV[2].to_i if ARGV.length == 3
 
 seen_tags = 0
 
@@ -14,6 +16,12 @@ while chunk = STDIN.gets('>') do
 	if seen_tags > 0 and chunk.end_with?(closetag) then
 		print chunk[0..-1*closetag.length-1]
 		seen_tags -= 1
+
+		if match > 0 and seen_tags == 0 then
+			match -= 1
+			exit unless match > 0
+		end
+
 		next
 	end
 	print chunk if seen_tags > 0
